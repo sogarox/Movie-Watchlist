fb995379dbc3436af3585489f771c3de0534da2c
Samuel Barrios <samuelbc650@gmail.com>
2026-09-10 20:29:57 -0500

    avance10092026

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inputField = document.getElementById("searchMovies")
 
 secondHalf = document.getElementById("secondHalf")
-
+let watchlist = []
 
 
 document.addEventListener("click", async (e) => {
@@ -18,8 +18,11 @@ document.addEventListener("click", async (e) => {
                 })
             )
             console.log(detailedMovies)
-            const moviesList = detailedMovies.map(detailedMovie => (
-                `<div class="movie-box" id="movieId-${detailedMovie.imdbID}">
+            const moviesList = detailedMovies.map(detailedMovie => {
+
+                const isInWatchlist = watchlist.includes(detailedMovie.imdbID)
+
+                return `<div class="movie-box" id="movieId-${detailedMovie.imdbID}">
                 <img src="${detailedMovie.Poster}"
                     alt="">
                 <div class="title-and-rating" id="titleAndRating">
@@ -31,13 +34,13 @@ document.addEventListener("click", async (e) => {
                     <p>${detailedMovie.Runtime}</p>
                     <p>${detailedMovie.Genre}</p>
                     <button class="add-to-watchlist" id="addToWatchlist" data-add="${detailedMovie.imdbID}">
-                        <svg class="add-svg" id="addSvg" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                        <svg class="add-svg" id="addSvg" style="display: ${isInWatchlist ? "none" : "block"}" width="16" height="16" viewBox="0 0 16 16" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z"
                                 fill="#111827" />
                         </svg>
-                        <svg class="remove-svg" id="removeSvg" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                        <svg class="remove-svg" id="removeSvg" style="display: ${isInWatchlist ? "none" : "block"}"  width="16" height="16" viewBox="0 0 16 16" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H5Z"
@@ -52,7 +55,10 @@ document.addEventListener("click", async (e) => {
                     </p>
                 </div>
             </div>`
-            ))
+    })
+
+
+
             secondHalf.innerHTML = moviesList.join("")
             secondHalf.style.justifyContent = "start"
             secondHalf.style.paddingTop = "50px"
@@ -65,6 +71,17 @@ document.addEventListener("click", async (e) => {
     const watchlistButton = e.target.closest(".add-to-watchlist")
 
     if (!watchlistButton) return
+
+    const imdbID = watchlistButton.dataset.add
+    if (!watchlist.includes(watchlistButton.dataset.add)) {
+        watchlist.push(imdbID)
+    } else {
+        watchlist.splice(watchlist.indexOf(imdbID), 1)
+    }
+
+
+
+    console.log(watchlist)
 
     addSvg = watchlistButton.querySelector(".add-svg")
     removeSvg = watchlistButton.querySelector(".remove-svg")
